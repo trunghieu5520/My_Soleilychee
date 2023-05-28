aaac5db4f11f6d0b937bee98c87810c2eec40aa2
Nguyễn Trung Hieu <78343616+trunghieu5520@users.noreply.github.com>
2023-05-28 22:56:13 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Chào Nhung thúi!',
+    introDesc: `Sao hôm nay lại cứ ngẩn ngơ thế này
+    Sao mà bây giờ đây vũ trụ em chỉ thấy anh thôi đấy
+    Bắt đền đi trả lại tâm trí đây
+    Meo meo meo meo trả lại tâm trí tôi đây`,
+    btnIntro: 'Đừng bấm vào đâyyy',
+    title: 'Lỡ bấm rồi là phải làm bạn gái mình thôi hầyyyy?',
+    desc: ' Làm người iuuuu Hiu nhéee =))))',
+    btnYes: 'Tớ đồng ý <3',
+    btnNo: 'Không đâuuuu',
+    question:'Tại sao b đồng vậyyyy :>',
+    btnReply: 'BẤM VÀO ĐÂYYYY',
+    reply: 'Vì cậuuu rất đẹp trai <33333333 =))))',
+    mess: 'Xin cam onnn :>>>',
+    messDesc: 'Cho to lam mot phan muc tieu cung cau nhe :>',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/hieu.nguyen.5520/'
+}
